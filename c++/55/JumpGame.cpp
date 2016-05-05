ed77845ed173a222c4a6a1c4e52fa7c38482210c
millerwu <wumiaoliang@gmail.com>
2016-05-05 20:41:52 +0800

    add 55

---
+++ c++/55/JumpGame.cpp
@@ -0,0 +1,36 @@
+//source: https://leetcode.com/problems/jump-game/
+
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+*/
+
+#include <iostream>
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+		int max_reach = 0;
+		for(int i = 0; i < nums.size();)
+		{
+			max_reach
+			for(int j = i; j < max_reach; j++)
+			{
+				
+			}
+		}
+    }
+};
+
+int main ()
+{
+	return 0;
+}
