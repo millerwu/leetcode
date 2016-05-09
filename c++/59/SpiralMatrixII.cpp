52b68353811dece32cc26ddcf87d64c2091f11b8
millerwu <wumiaoliang@gmail.com>
2016-05-09 09:32:39 +0800

    add 59 problem

---
+++ c++/59/SpiralMatrixII.cpp
@@ -0,0 +1,29 @@
+//source: https://leetcode.com/problems/spiral-matrix-ii/
+
+/*
+Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+For example,
+Given n = 3,
+
+You should return the following matrix:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+*/
+
+#include <iostream>
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        
+    }
+};
+
+int main ()
+{
+	return 0;
+}
