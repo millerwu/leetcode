c657355db1bc039a4a4694126a8df58a77ff1adf
millerwu <wumiaoliang@gmail.com>
2016-05-20 16:32:53 +0800

    add new problem

---
+++ java/5-LongestPalindromicSubstring/Solution.java
@@ -0,0 +1,19 @@
+//source:https://leetcode.com/problems/longest-palindromic-substring/
+//author:
+//data:
+
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+    
---
+++ java/7-ReverseInteger/Solution.java
@@ -0,0 +1,22 @@
+//source:https://leetcode.com/problems/reverse-integer/
+//author:
+//data:
+
+/*
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+*/
+
+public class Solution {
+    public int reverse(int x) {
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+    
