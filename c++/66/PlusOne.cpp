64ed73242c340e030299cef7ed12d1fc95bccdcd
millerwu <wumiaoliang@gmail.com>
2016-07-13 17:18:44 +0800

    add 66 solution cpp

--- c++/66/PlusOne.cpp
+++ c++/66/PlusOne.cpp
@@ -13,15 +13,50 @@ Show Similar Problems
 */
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        
+    static vector<int> plusOne(vector<int>& digits) {
+        if (digits.size() == 0)
+        {
+            return digits;
+        }
+        int carry = 1;
+        for (int i = digits.size()-1; i >= 0; i--)
+        {
+            int temp = digits[i]+carry;
+            carry = temp/10;
+            digits[i] = temp%10;
+        }
+        if (carry)
+        {
+            digits.insert(digits.begin(), carry);
+        }
+        return digits;
     }
 };
 
+void printVector(vector<int> test)
+{
+	for (int i = 0; i < test.size(); ++i)
+	{
+		cout << " " << test[i];
+	}
+    cout << endl;
+}
+
 int main ()
 {
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(9);
+
+    vector<int> res = Solution::plusOne(v);
+
+    printVector(res);
 	return 0;
 }
