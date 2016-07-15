2cdf91899cc16609f62e30eb18939daab927f325
millerwu <wumiaoliang@gmail.com>
2016-07-15 11:19:45 +0800

    add some DP problem solution cpp

--- c++/70/ClimbingStairs.cpp
+++ c++/70/ClimbingStairs.cpp
@@ -9,15 +9,32 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 */
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        
+    static int climbStairs(int n) {
+        if (n==0)
+            return 0;
+        vector<int> m(n+1, 0);
+        for (int i = 0; i< m.size(); i++)
+        {
+            if (i < 2)
+            {
+                m[i] = 1;
+            } else {
+                m[i] = m[i-1] + m[i-2];
+            }
+        }
+        return m[n];
     }
 };
 
-int main ()
+int main (int argc, char** argv)
 {
+    int n = atoi(argv[1]);
+    cout << n << " need " << Solution::climbStairs(n) << " steps" << endl;
 	return 0;
 }
---
+++ c++/85/MaximalRectangle.cpp
@@ -0,0 +1,17 @@
+//source: https://leetcode.com/problems/maximal-rectangle/
+
+/*
+
+*/
+
+#include <iostream>
+
+class Solution {
+public:
+    
+};
+
+int main ()
+{
+	return 0;
+}
