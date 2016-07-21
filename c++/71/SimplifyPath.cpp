93cf9e166d9fb43ea5eaa6f1689ecfb086cca954
millerwu <wumiaoliang@gmail.com>
2016-07-21 17:31:23 +0800

    add some problems

--- c++/71/SimplifyPath.cpp
+++ c++/71/SimplifyPath.cpp
@@ -10,15 +10,68 @@ path = "/a/./b/../../c/", => "/c"
 */
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-    string simplifyPath(string path) {
-        
+    static std::string simplifyPath(std::string path) {
+        if (path.size() == 0)
+        {
+            return path;
+        }
+        cout << path << endl;
+        int index = 0;
+        vector<std::string> fname;
+        while (index < path.size())
+        {
+            if (path[index] == '/')
+            {
+                if (fname.size()==0 || fname.back().size() > 0)
+                {
+                    fname.push_back("");
+                }
+                index++;
+                continue;
+            }
+            if (path[index] == '.')
+            {
+                index++;
+                if (path[index] == '.')
+                {
+                    if (fname.size()>0)
+                    {
+                        fname.pop_back();
+                        if (fname.size()>0)
+                        {
+                            fname.pop_back();
+                        }
+                    }
+                    index++;
+                }
+                continue;
+            }
+            fname.back().insert(fname.back().end(), path[index]);
+            index++;
+        }
+        cout << endl;
+        std::string res = "";
+        for (int i = 0; i < fname.size(); i++)
+        {
+            if (!fname[i].empty())
+             res = res+"/"+fname[i];
+        }
+        return res;
     }
 };
 
+#define TEST(X) cout<<X << "simple path is" << Solution::simplifyPath(X) << endl;
+
 int main ()
 {
+    std::string path = Solution::simplifyPath("/home//foo/");
+    cout << path << endl;
 	return 0;
 }
