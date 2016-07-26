f8731ddbbc1f19194e3a87de364245b50c43dfea
millerwu <wumiaoliang@gmail.com>
2016-07-26 17:17:08 +0800

    add 69 & 7s solution cpp

--- c++/69/Sqrt(x).cpp
+++ c++/69/Sqrt(x).cpp
@@ -9,14 +9,35 @@ Compute and return the square root of x.
 
 #include <iostream>
 
+using namespace std;
+
 class Solution {
 public:
-    int mySqrt(int x) {
-        
+    static int mySqrt(int x) {
+        long long l = 0;
+        long long r = (x/2)+1;
+        while (l <= r) 
+        {
+            long long mid = (l+r)/2;
+            long long s = mid * mid;
+            if (s == x)
+            {
+                return mid;
+            } 
+            else if (s < x) {
+                l = mid + 1;
+            } else {
+                r = mid - 1; 
+            }
+        }
+        return r;     
     }
 };
+#define TEST_SQRT(X) cout << X << " sqrt is " << Solution::mySqrt(X)<< endl;
 
-int main ()
+int main (int argc, char** argv)
 {
+    int x = atoi(argv[1]);
+    TEST_SQRT(x);
 	return 0;
 }
--- c++/73/SetMatrixZeroes.cpp
+++ c++/73/SetMatrixZeroes.cpp
@@ -1,17 +1,82 @@
-//source: 
+//source: https://leetcode.com/problems/set-matrix-zeroes/
 
 /*
-
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
 */
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-    
+    static void setZeroes(vector<vector<int> >& matrix) {
+		if (matrix.size() == 0)
+			return;
+		vector<int> row;
+		vector<int> col;
+
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			for (int j = 0; j < matrix[i].size(); j++)
+			{
+				if (matrix[i][j] == 0)
+				{
+					row.push_back(i);
+					col.push_back(j);
+				}
+			}
+		}
+        for (int i = 0; i < row.size(); i++)
+		{
+			for (int j = 0; j < matrix[0].size(); j++)
+			{
+				matrix[row[i]][j] = 0;
+			}
+		}
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			for (int j = 0; j < col.size(); j++)
+			{
+				matrix[i][col[j]] = 0;
+			}
+		}
+		return;
+    }
 };
+
+void printVector(vector< vector<int> > test)
+{
+	for (int i = 0; i < test.size(); ++i)
+	{
+		for (int j = 0; j < test[i].size(); ++j)
+		{
+			cout << " " << test[i][j];
+		}
+		cout << endl;
+	}
+}
 
 int main ()
 {
+	vector< vector<int> > test;
+	vector<int> t;
+	int temp = 0;
+	for (int i = 0; i < 10; i ++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			t.push_back(temp++);
+		}
+		test.push_back(t);
+		t.clear();
+	}
+	printVector(test);
+
+	Solution::setZeroes(test);
+
+	printVector(test);
+
 	return 0;
 }
