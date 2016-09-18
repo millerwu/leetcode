cdfede88b9d62e1722ede110745a6be9c020b631
millerwu <wumiaoliang@gmail.com>
2016-09-18 22:14:40 +0800

    add solution

--- c++/81/SearchinRotatedSortedArrayII.cpp
+++ c++/81/SearchinRotatedSortedArrayII.cpp
@@ -10,10 +10,12 @@ Write a function to determine if a given target is in the array.
 */
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
+   	static bool search(vector<int>& nums, int target) {
     	if (nums.size()<=0)
     	{
     		return false;
@@ -22,11 +24,11 @@ public:
     	int end = nums.size()-1;
 
     	int mid = (start+end)/2;
-    	while (start<end)
+    	while (start<end-1)
     	{
     		if (nums[start]<nums[mid])
     		{
-				if (target>=nums[start] && target <= nums[mid])
+			if (target>=nums[start] && target <= nums[mid])
     			{
     				end = mid;
     				mid = (start+end)/2;
@@ -34,25 +36,83 @@ public:
     				{
     					if (nums[mid] >= target)
     					{
-							end = mid;    						
+						end = mid; 
     					} else {
     						start = mid+1;
     					}
+    					mid = (start+end)/2;
     				}
-    				return nums[end]==target;
+    				if (nums[end]==target)
+    				 {
+    				 	return true;
+    				 } else {
+    				 	return false;
+    				 }
     			} 
     			start = mid;
     			mid = (start+end)/2;
     			continue;
+    		} else if (nums[mid] < nums[end]){
+    			if (target>=nums[mid] && target <=nums[end])
+    			{
+    				start=mid;
+    				mid = (start+end)/2;
+    				while(start < end)
+    				{
+    					if (nums[mid]>=target)
+    					{
+    						end = mid;
+    					} else {
+    						start = mid+1;
+    					}
+    					mid = (start+end)/2;
+    				}
+    				if (nums[end]==target)
+    				 {
+    				 	return true;
+    				 } else {
+    				 	return false;
+    				 }
+    			}
+    			end = mid;
+    			mid = (start+end)/2;
+    			continue;
     		} else {
-
+    			if (nums[start]==target)
+    			{
+    				return true;
+    			}
+    			start++;
+    			mid = (start+end)/2;
     		}
+    		
     	}
-        
+	if (nums[start]==target || nums[end] == target)
+	{
+		return true;
+	} 
+	return false;
     }
 };
 
 int main ()
 {
+	std::vector<int> v;
+	// v.push_back(1);
+	// v.push_back(5);
+	// v.push_back(6);
+	// v.push_back(7);
+	// v.push_back(0);
+	// v.push_back(1);
+	// v.push_back(2);
+
+	v.push_back(1);
+	v.push_back(1);
+	v.push_back(3);
+	v.push_back(1);
+	int i = 0;
+	cin >> i;
+	bool r = Solution::search(v, i);
+	cout << "res = " << r << endl;
 	return 0;
 }
--- c++/86/PartitionList.cpp
+++ c++/86/PartitionList.cpp
@@ -11,23 +11,74 @@ return 1->2->2->4->3->5.
 */
 
 #include <iostream>
+#include <vector>
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+using namespace std;
+
+  // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+ void printList(ListNode* node)
+{
+	while (node!=NULL)
+	{
+		cout<< node->val << " ";
+		node = node->next;
+	}
+	cout << endl;
+
+}
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x) {
-        
+   	static  ListNode* partition(ListNode* head, int x) {
+    	ListNode bhead(0);
+    	ListNode lhead(0);
+    	ListNode* bNode = &bhead;
+    	ListNode* lNode = &lhead;
+    	ListNode* n = head;
+    	while(n != NULL)
+    	{
+ 		// cout << "value = " << n->val << endl;
+    		if (n->val < x)
+    		{
+    			bNode->next = n;
+    			bNode = bNode->next;
+    			// cout << "BNode value = " << bNode->val << endl;
+    		} else {
+    			lNode->next = n;
+    			lNode = lNode->next;
+    			// cout << "lNode value = " << lNode->val << endl;
+    		}
+    		n = n->next;
+    	}
+    	lNode->next = NULL;
+    	// printList(bhead.next);
+    	// printList(lhead.next);
+    	bNode->next = lhead.next;
+    	
+    	return bhead.next;
     }
 };
 
 int main ()
 {
+	ListNode l1(1);
+	ListNode l2(4);
+	ListNode l3(3);
+	ListNode l4(2);
+	ListNode l5(5);
+	ListNode l6(2);
+	l1.next = &l2;
+	l2.next = &l3;
+	l3.next = &l4;
+	l4.next = &l5;
+	l5.next = &l6;
+
+	ListNode* r = Solution::partition(&l1, 3);
+	printList(r);
 	return 0;
 }
