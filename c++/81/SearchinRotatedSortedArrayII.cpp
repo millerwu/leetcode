929c37f9ebf890ca566ea48130fcb49debe4b86b
millerwu <wumiaoliang@gmail.com>
2016-09-18 18:24:07 +0800

    update 81

--- c++/81/SearchinRotatedSortedArrayII.cpp
+++ c++/81/SearchinRotatedSortedArrayII.cpp
@@ -14,7 +14,40 @@ Write a function to determine if a given target is in the array.
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-    	
+    	if (nums.size()<=0)
+    	{
+    		return false;
+    	}
+    	int start = 0;
+    	int end = nums.size()-1;
+
+    	int mid = (start+end)/2;
+    	while (start<end)
+    	{
+    		if (nums[start]<nums[mid])
+    		{
+				if (target>=nums[start] && target <= nums[mid])
+    			{
+    				end = mid;
+    				mid = (start+end)/2;
+    				while (start < end)
+    				{
+    					if (nums[mid] >= target)
+    					{
+							end = mid;    						
+    					} else {
+    						start = mid+1;
+    					}
+    				}
+    				return nums[end]==target;
+    			} 
+    			start = mid;
+    			mid = (start+end)/2;
+    			continue;
+    		} else {
+
+    		}
+    	}
         
     }
 };
