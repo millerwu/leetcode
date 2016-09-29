61b45163c07a927ac1234f10615b9c90224e42fa
millerwu <wumiaoliang@gmail.com>
2016-09-29 21:35:27 +0800

    add 99 solution

--- c++/99/RecoverBinarySearchTree.cpp
+++ c++/99/RecoverBinarySearchTree.cpp
@@ -11,24 +11,41 @@ A solution using O(n) space is pretty straight forward. Could you devise a const
 */
 
 #include <iostream>
+#include <vector>
 
+using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
- 
-class Solution {
-public:
-    void recoverTree(TreeNode* root) {
-        
-    }
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
+void inorderTravel(TreeNode* node, std::vector<TreeNode*>& list, std::vector<int>& list_val)
+{
+	if (node == NULL)
+	{
+		return;
+	}
+	inorderTravel(node->left, list, list_val);
+	list.push_back(node);
+	list_val.push_back(node->val);
+	inorderTravel(node->right, list, list_val);
+}
+ 
+void recoverTree(TreeNode* root) {
+	vector<TreeNode*> list_node;
+	vector<int> list_val;
+	inorderTravel(root, list_node, list_val);
+	sort(list_val.begin(), list_val.end());
+	for (int i = 0; i < list_node.size(); ++i)
+	{
+		list_node[i]->val = list_val[i];
+	}
+	return;
+}
 
 int main ()
 {
