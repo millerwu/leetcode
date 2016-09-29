064dd15a2bc68baeb58a3e6c9d43a2aaa0a2d6f6
millerwu <wumiaoliang@gmail.com>
2016-09-29 22:33:04 +0800

    add 101, 102 solution

--- c++/101/SymmetricTree.cpp
+++ c++/101/SymmetricTree.cpp
@@ -23,8 +23,64 @@ But the following [1,2,2,null,3,null,3] is not:
 
 using namespace std;
 
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool isSymmetricHelper(TreeNode* n1, TreeNode* n2)
+{
+	if (n1 == NULL && n2 == NULL)
+	{
+		return true;
+	} else if (n1 == NULL || n2 == NULL)
+	{
+	  return false;
+	}
+	if (n1->val == n2->val && isSymmetricHelper(n1->left, n2->right) && isSymmetricHelper(n1->right, n2->left))
+	{
+		return true;
+	}
+	return false;
+}
+
 bool isSymmetric(TreeNode* root) {
-        
+	if (root == NULL)
+	{
+		return true;
+	}
+	return isSymmetricHelper(root->left, root->right);
+}
+
+TreeNode* createTree(int a[], n) {
+	if (n <= 0)
+		return NULL;
+	TreeNode** tree = new TreeNode*[n];
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (a[i] == 0)
+		{
+			tree[i] == NULL;
+		} else {
+			tree[i] = new TreeNode(a[i]);
+		}
+	}
+	int pos = 1;
+	for (int i = 0; i<n && pos<n ; ++i)
+	{
+		if (tree[i] != NULL)
+		{
+			tree[i]->left = tree[pos++];
+			if (pos < n)
+			{
+				tree[i]->right = tree[pos++];
+			}
+		}
+	}
+	return tree[0];
 }
 
 int main ()
--- c++/102/BinaryTreeLevelOrderTraversal.cpp
+++ c++/102/BinaryTreeLevelOrderTraversal.cpp
@@ -30,11 +30,103 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-vector<vector<int>> levelOrder(TreeNode* root) {
-        
+vector<vector<int> > levelOrder(TreeNode* root) {
+  vector<vector<int> > res;
+  if (root == NULL)
+  {
+    return res;
+  }
+  vector<TreeNode*> parent;
+  vector<TreeNode*> node;
+  vector<int> val;
+  parent.push_back(root);
+  while (parent.size() > 0)
+  {
+    while (parent.size() > 0)
+    {
+      TreeNode* r = parent.front();
+      val.push_back(r->val);
+      parent.erase(parent.begin());
+      if (r->left != NULL)
+      {
+        node.push_back(r->left);
+      }
+      if (r->right != NULL)
+      {
+        node.push_back(r->right);
+      }
+    }
+    res.push_back(val);
+    val.clear();
+    parent = node;
+    node.clear();
+  }
+  return res;     
+}
+
+TreeNode* createTree(int a[], int n) {
+  if (n <= 0)
+    return NULL;
+  TreeNode** tree = new TreeNode*[n];
+  // cout << "createTree in" << endl;
+
+  for (int i = 0; i < n; ++i)
+  {
+    if (a[i] == 0)
+    {
+      tree[i] == NULL;
+    } else {
+      tree[i] = new TreeNode(a[i]);
+    }
+  }
+  // cout << "createTree mid" << endl;
+  int pos = 1;
+  for (int i = 0; i<n && pos<n ; ++i)
+  {
+    if (tree[i] != NULL)
+    {
+      tree[i]->left = tree[pos++];
+      if (pos < n)
+      {
+        tree[i]->right = tree[pos++];
+      }
+    }
+  }
+  // cout << "createTree end" << endl;
+  return tree[0];
+}
+
+void printTree(TreeNode* node)
+{
+  if (node == NULL)
+  {
+    return;
+  }
+  // cout<< "printTree" << endl;
+  printTree(node->left);
+  cout<< node->val << " ";
+  printTree(node->right);
+}
+
+void printVector(vector<vector<int> > v)
+{
+  for (int i = 0; i < v.size(); ++i)
+  {
+    for (int j = 0; j < v[i].size() ; ++j)
+    {
+      cout << v[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
 
 int main ()
 {
+  int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 9};
+  TreeNode* node = createTree(a, sizeof(a)/sizeof(int));
+  printTree(node);
+  cout<< "printTree out" << endl;
+  vector<vector<int> > res = levelOrder(node);
+  printVector(res);
 	return 0;
 }
