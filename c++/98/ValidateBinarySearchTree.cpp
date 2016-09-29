d1e87e4b1ea614d3f2de7b4cd69cd0576994dbd8
millerwu <wumiaoliang@gmail.com>
2016-09-29 10:38:32 +0800

    add 98 solution

--- c++/98/ValidateBinarySearchTree.cpp
+++ c++/98/ValidateBinarySearchTree.cpp
@@ -21,26 +21,98 @@ Binary tree [1,2,3], return false.
 */
 
 #include <iostream>
+#include <vector>
 
+using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
- 
-class Solution {
-public:
-    bool isValidBST(TreeNode* root) {
-        
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};  
+
+// use stack to store node;
+// The solution is the same with inorder Traversal
+bool isValidBST(TreeNode* root) {
+	if (root == NULL)
+	{
+		return true;
+	}
+	vector<TreeNode*> stack;
+	TreeNode* node = root;
+	int last_val = INT_MIN;
+	bool first = true;
+	stack.push_back(node);
+	while (node != NULL && stack.size() > 0)
+	{	
+		if (node->left != NULL)
+		{
+			node = node->left;
+			stack.push_back(node);
+			continue;
+		} else {
+			while (stack.size() > 0)
+			{
+				TreeNode* n = stack.back();
+				stack.pop_back();
+				if (n->val > last_val || first)
+				{
+					first = false;
+					last_val = n->val;
+				} else{
+					return false;
+				}
+				if (n->right != NULL)
+				{
+					node = n->right;
+					stack.push_back(node);
+					break;
+				}				
+			}
+		}
+	}
+	return true;
+}
+
+TreeNode* createTree(int a[], int n)
+{
+    if (n<=0) return NULL;
+
+    TreeNode **tree = new TreeNode*[n];
+
+    for(int i=0; i<n; i++) {
+        if (a[i]==0 ){
+            tree[i] = NULL;
+            continue;
+        }
+        tree[i] = new TreeNode(a[i]);
     }
-};
+    int pos=1;
+    for(int i=0; i<n && pos<n; i++) {
+        if (tree[i]){
+            tree[i]->left = tree[pos++];
+            if (pos<n){
+                tree[i]->right = tree[pos++];
+            }
+        }
+    }
+    return tree[0];
+}
+
 
 int main ()
 {
-	return 0;
+	cout << isValidBST(NULL) << endl;
+
+  int a[]={1,1};
+  cout << isValidBST(createTree(a, sizeof(a)/sizeof(int))) << endl;
+  
+  int b[]={4,2,6,1,7,5,7};
+  cout << isValidBST(createTree(b, sizeof(b)/sizeof(int))) << endl;
+
+  int c[]={4,2,6,1,3,5,7};
+  cout << isValidBST(createTree(c, sizeof(c)/sizeof(int))) << endl;
+  return 0;
 }
