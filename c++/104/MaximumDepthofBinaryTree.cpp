17a216da5f87be404cd1f2b898b1b5ba4b054af2
millerwu <wumiaoliang@gmail.com>
2016-09-30 16:17:23 +0800

    add 103 104 solution

--- c++/104/MaximumDepthofBinaryTree.cpp
+++ c++/104/MaximumDepthofBinaryTree.cpp
@@ -18,8 +18,23 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+void maxDepthHelper(TreeNode* root, int depth, int& res)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	depth++;
+	res = res > depth ? res : depth;
+	maxDepthHelper(root->left, depth, res);
+	maxDepthHelper(root->right, depth, res);
+	return;
+}
+
 int maxDepth(TreeNode* root) {
-        
+	int res = 0;
+	maxDepthHelper(root, 0, res);
+	return res;
 }
 
 int main ()
