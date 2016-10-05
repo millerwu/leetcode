32d78b7740d6d2b534f63ee3e2c97b9e50b8c1da
millerwu <wumiaoliang@gmail.com>
2016-10-05 22:09:09 +0800

    add 107 solution

--- c++/107/BinaryTreeLevelOrderTraversalII.cpp
+++ c++/107/BinaryTreeLevelOrderTraversalII.cpp
@@ -22,6 +22,7 @@ return its bottom-up level order traversal as:
 
 #include <iostream>
 #include <vector>
+#include <algorithm>    // std::reverse
 
 using namespace std;
 
@@ -32,9 +33,48 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-vector<vector<int>> levelOrderBottom(TreeNode* root) {
-    
-}
+vector<vector<int> > levelOrderBottom(TreeNode* root) {
+  if (root == NULL)
+  {
+    return res;
+  }
+  vector<TreeNode*> parent;
+  vector<int> node_res;
+  vector<TreeNode*> back_up;
+  TreeNode* node;
+  parent.push_back(root);
+  node_res.push_back(root->val);
+  while (parent.size() > 0)
+  {
+     if(node_res.size() > 0)
+        res.push_back(node_res);
+    node_res.clear();    
+    while (parent.size() > 0)
+    {
+      node = parent.front();
+    //   node_res.clear();
+      if (node->left != NULL)
+       {
+         back_up.push_back(node->left);
+         node_res.push_back(node->left->val);
+         cout << "left val = " << node->left->val << endl;
+       } 
+       if (node->right != NULL)
+       {
+         back_up.push_back(node->right);
+         node_res.push_back(node->right->val);
+         cout << "right val = " << node->right->val << endl;
+       }
+       parent.erase(parent.begin());
+    }
+    parent = back_up;
+    back_up.clear();
+  }
+  if (node_res.size() > 0)
+     res.push_back(node_res);
+  std::reverse(res.begin(), res.end());
+  return res;
+    }
 
 int main ()
 {
