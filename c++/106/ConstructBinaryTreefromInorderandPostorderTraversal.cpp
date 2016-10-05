3f427dd13f38a29c9dd353ac5796ce509eb351dd
millerwu <wumiaoliang@gmail.com>
2016-10-05 21:34:08 +0800

    add 106 solution

--- c++/106/ConstructBinaryTreefromInorderandPostorderTraversal.cpp
+++ c++/106/ConstructBinaryTreefromInorderandPostorderTraversal.cpp
@@ -6,6 +6,7 @@ Given inorder and postorder traversal of a tree, construct the binary tree.
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -16,11 +17,33 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+TreeNode* buildTreeHelper(vector<int>& inorder, int in_start, int in_size, 
+							vector<int>& postorder, int post_start, int post_size,
+								map<int, int>& map) {
+	if (post_size < 1)
+	{
+		return NULL;
+	}
+	TreeNode* root = new TreeNode(postorder[post_start+post_size-1]);
+	int root_index = map.find(root->val)->second;
+	int left_size = root_index - in_start;
+	int right_size = in_size - (left_size) - 1;
+	root->left = buildTreeHelper(inorder, in_start, left_size, postorder, post_start, left_size, map);
+	root->right = buildTreeHelper(inorder, root_index+1, right_size, postorder, post_start+left_size, right_size, map);
+	return root; 
+}
+
 TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
+	map<int, int> index_map;
+	for (int i = 0; i < inorder.size(); ++i)
+	{
+		index_map.insert(pair<int, int>(inorder[i], i));
+	}
+	return buildTreeHelper(inorder, 0, inorder.size(), postorder, 0, postorder.size(), index_map);
 }
 
 int main ()
 {
 	return 0;
 }
+
