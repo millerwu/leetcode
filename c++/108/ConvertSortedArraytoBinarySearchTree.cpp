aefedaf64422c3afacf00c8aefafafe77fc02e84
millerwu <wumiaoliang@gmail.com>
2016-10-08 14:06:48 +0800

    add 108 solution

--- c++/108/ConvertSortedArraytoBinarySearchTree.cpp
+++ c++/108/ConvertSortedArraytoBinarySearchTree.cpp
@@ -16,11 +16,40 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+TreeNode* sortedArrayToBSTHelper(vector<int>& nums, int start, int end) {
+	if (start > end)
+	{
+		return NULL;
+	}
+	int mid = (start + end) / 2;
+	TreeNode* node = new TreeNode(nums[mid]);
+	node->left = sortedArrayToBSTHelper(nums, start, mid-1);
+	node->right = sortedArrayToBSTHelper(nums, mid+1, end);
+	return node;
+}
+
 TreeNode* sortedArrayToBST(vector<int>& nums) {
-    
+	if (nums.size() <= 0)
+	{
+		return NULL;
+	}
+	return sortedArrayToBSTHelper(nums, 0, nums.size()-1);
+}
+
+void printTree(TreeNode* root) {
+	if (root == nullptr)
+	{
+		return;
+	}
+	cout << root->val << " " << endl;
+	printTree(root->left);
+	printTree(root->right);
 }
 
 int main ()
 {
+	vector<int> nums = {1, 2, 3, 4, 5};
+	TreeNode* node = sortedArrayToBST(nums);
+	printTree(node);
 	return 0;
 }
