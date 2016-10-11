e584409382cc61a5ab7c2f435f860dba7f03f73c
millerwu <wumiaoliang@gmail.com>
2016-10-11 16:10:25 +0800

    add117solution

--- c++/117/PopulatingNextRightPointersinEachNodeII.cpp
+++ c++/117/PopulatingNextRightPointersinEachNodeII.cpp
@@ -35,7 +35,58 @@ struct TreeLinkNode {
 };
 
 void connect(TreeLinkNode *root) {
-    
+    if (root == NULL || (root->left == NULL && root->right == NULL))
+    {
+      return NULL;
+    }
+    TreeLinkNode* node = NULL;
+    TreeLinkNode* root_temp = root;
+    while (node == NULL && root_temp != NULL) {
+      node = root_temp->left;
+      if (node == NULL)
+      {
+        node = root_temp->right;
+        if (node == NULL)
+        {
+          root_temp = root_temp->next;
+        }
+      }
+    }
+    if (root_temp != NULL)
+    {
+      TreeLinkNode* next_node = NULL;
+      if (node == root_temp->left)
+      {
+        next_node = root_temp->right;
+        if (next_node == NULL)
+        {
+          root_temp = root_temp->next;
+        }
+      } else {
+        root_temp = root_temp->next;
+      }
+      while (next_node == NULL && root_temp != NULL){
+        next_node = root_temp->left;
+        if (next_node == NULL)
+        {
+          next_node = root_temp->right;
+          if (next_node == NULL)
+          {
+            root_temp = root_temp->next;
+          }
+        }
+      }
+      node->next = next_node;
+      if (node == root_temp->left && next_node == root_temp->right)
+      {
+        /* code */
+        
+      }
+    } else {
+      node->next = NULL;
+    }
+    connect(root->left);
+    connect(root->right);
 }
 
 int main ()
