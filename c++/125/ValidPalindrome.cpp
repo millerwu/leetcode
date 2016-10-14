9fa0e8bf54188bbfd17b65e85555a536f49787cc
millerwu <wumiaoliang@gmail.com>
2016-10-14 22:01:02 +0800

    add125

--- c++/125/ValidPalindrome.cpp
+++ c++/125/ValidPalindrome.cpp
@@ -17,14 +17,44 @@ For the purpose of this problem, we define empty string as valid palindrome.
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <stdlib.h>
 
 using namespace std;
 
 bool isPalindrome(string s) {
-    
+	int start = 0;
+	int end = s.size()-1;
+	while (start < end) {
+		if (!isalnum(s[start]))
+		{
+			++start;
+			continue;
+		}
+		if (!isalnum(s[end]))
+		{
+			--end;
+			continue;
+		}
+		if (s[start] == s[end] || (isalpha(s[start]) && isalpha(s[end]) && abs(s[start] - s[end]) == 32))
+		{
+			++start;
+			--end;
+			continue;
+		} else {
+			return false;
+		}
+	}
+  return true;
 }			
 
 int main ()
 {
+	string s = "aaa";
+	string s1 = "A man, a plan, a canal: Panama";
+	string s2 = "race a car";
+	cout << isPalindrome(s) << endl;
+	cout << isPalindrome(s1) << endl;
+	cout << isPalindrome(s2) << endl;
 	return 0;
 }
