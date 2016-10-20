2e955992ef528c54e20f68ffb2009f19c94da460
millerwu <wumiaoliang@gmail.com>
2016-10-20 17:58:18 +0800

    add127

--- c++/127/WordLadder.cpp
+++ c++/127/WordLadder.cpp
@@ -22,14 +22,71 @@ All words contain only lowercase alphabetic characters.
 
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-    
+int differChar(string s1, string s2) {
+	int count = 0;
+	for (int i = 0; i < s1.size(); i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			++count;
+		}
+	}
+	return count;
+} 
+
+void ladderLengthHelper(string beginWord, string endWord, unordered_set<string>& wordList, vector<string>& stackList, int& res) {
+	if (differChar(beginWord, endWord) <= 1)
+	{
+		int temp_res = stackList.size() + 2;
+		res = res > temp_res ? temp_res : res;
+		return;
+	}
+	for (auto word : wordList)	
+	{
+		cout << "word = " << word << endl;
+		if (differChar(beginWord, word) == 1)
+		{
+			cout << "beginWord = " << beginWord << endl;
+			wordList.erase(word);
+			cout << "line = " << __LINE__ << endl;
+			stackList.push_back(word);
+			cout << "line = " << __LINE__ << endl;
+			ladderLengthHelper(word, endWord, wordList, stackList, res);
+			cout << "line = " << __LINE__ << endl;
+			stackList.pop_back();	
+			cout << "line = " << __LINE__ << endl;
+			cout << "word1 =  " << word << endl;
+			wordList.insert(word);
+			cout << "line = " << __LINE__ << endl;
+		}
+	}
 }
+
+int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+  int res = 0;
+  vector<string> outputList;
+  if (beginWord == endWord || wordList.size() <= 0)
+  {
+  	return res;
+  }
+  ladderLengthHelper(beginWord, endWord, wordList, outputList, res);
+  return res;  
+}
+
+
 
 int main ()
 {
+	string beginWord = "hit";
+	string endWord = "cog";
+	unordered_set<string> wordList = {"hot", "dot", "dog", "lot", "log"};
+	int res = ladderLength(beginWord, endWord, wordList);
+	cout << "res = " << res << endl;
 	return 0;
 }
