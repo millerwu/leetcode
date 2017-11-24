8096078ecfe5766b0ff025de325d85f79e859d22
millerwu <wumiaoliang@gmail.com>
2017-11-24 20:25:10 +0800

    update 134 & 136

--- c++/136/SingleNumber.cpp
+++ c++/136/SingleNumber.cpp
@@ -13,10 +13,17 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 using namespace std;
 
 int singleNumber(vector<int>& nums) {
-    
+	int result = nums[0];
+	for (int i = 1; i < nums.size(); ++i) {
+		result = result ^ nums[i];
+	}
+	return result;
 }
     
 int main ()
 {
+	vector<int> nums = {1, 1, 2, 2, 3, 3, 5};
+	int res = singleNumber(nums);
+	cout << "res: " << res << endl;
 	return 0;
 }
