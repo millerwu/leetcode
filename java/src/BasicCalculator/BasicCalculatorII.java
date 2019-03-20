cbc8ef385ea06bffef3e4e6dc9a2b25466c06fea
wu <wumiaoliang@gamil.com>
2019-03-20 09:19:29 +0800

    add BasicCalculatorII solution

--- java/src/BasicCalculator/BasicCalculatorII.java
+++ java/src/BasicCalculator/BasicCalculatorII.java
@@ -23,15 +23,74 @@ import java.util.Stack;
 
 public class BasicCalculatorII {
 
+    private int getNum(char c) {
+        return c - '0';
+    }
+
     public int calculate(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+
         Stack<Character> signStack = new Stack<>();
         Stack<Integer> numStack = new Stack<>();
+        int i = 0;
+        while (i < s.length()) {
+            if (Character.isDigit(s.charAt(i))) {
+                int num = getNum(s.charAt(i));
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    i++;
+                    num = num * 10 + getNum(s.charAt(i));
+                }
+                if (signStack.empty()) {
+                    numStack.push(num);
+                } else {
+                    if (signStack.peek() == '*') {
+                        signStack.pop();
+                        int topNum = numStack.pop();
+                        num = num * topNum;
+                    } else if (signStack.peek() == '/') {
+                        signStack.pop();
+                        int topNum = numStack.pop();
+                        num = topNum / num;
+                    }
+                    if (signStack.empty()) {
+                        numStack.push(num);
+                    } else {
+                        while (i + 1 < s.length() && s.charAt(i + 1) == ' ') {
+                            i++;
+                        }
+                        if (i + 1 < s.length() && (s.charAt(i + 1) == '*' || s.charAt(i + 1) == '/')) {
+                            numStack.push(num);
+                        } else {
+                            if (signStack.peek() == '+') {
+                                signStack.pop();
+                                int topNum = numStack.pop();
+                                num = topNum + num;
+                            } else if (signStack.peek() == '-') {
+                                signStack.pop();
+                                int topNum = numStack.pop();
+                                num = topNum - num;
+                            }
+                            numStack.push(num);
+                        }
+                    }
 
+                }
+            } else if (s.charAt(i) == ' ') {
 
-        return 0;
+            } else {
+                signStack.push(s.charAt(i));
+            }
+            i++;
+        }
+        return numStack.peek();
     }
 
     public static void main(String[] args) {
-
+        BasicCalculatorII basicCalculator = new BasicCalculatorII();
+        String testString = " 3+2*2";
+        int res = basicCalculator.calculate(testString);
+        System.out.println(res);
     }
 }
